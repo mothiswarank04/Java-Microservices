8507672221a6e8abbcac0f6cc264a06a239e5299
mothiswarank04 <mothiswarank04@gmail.com>
2026-06-15 23:19:57 +0530

    Update DepartmentController.java

--- department-service/src/main/java/com/microservices/department_service/controller/DepartmentController.java
+++ department-service/src/main/java/com/microservices/department_service/controller/DepartmentController.java
@@ -3,6 +3,7 @@ package com.microservices.department_service.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping("/department")
 public class DepartmentController {
 }
