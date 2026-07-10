c53a2fcd51b0376fa1b76c53c40251d4c678c022
mothiswarank04 <mothiswarank04@gmail.com>
2026-07-10 22:59:57 +0530

    Testing two DB connections

---
+++ dualDB/src/main/java/com/example/dualDB/DualDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.dualDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DualDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DualDbApplication.class, args);
+	}
+
+}
